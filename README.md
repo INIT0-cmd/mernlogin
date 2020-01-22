f91487c16480417a1f8c4eb9c943a58720a21c3c
sharat.kannatil <sharat.kannatil@vince.no>
2020-01-22 15:49:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Full-Stack-Login
+Full stack login REACT-REDUX app with SQL
+
+To run the application run the following in your cmd
+1)npm install
+2)npm run dev
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://sagar02:sagar02@sagars-gcaxq.mongodb.net/test?retryWrites=true&w=majority" ,
+    secretOrKey: "secret"
+  };
